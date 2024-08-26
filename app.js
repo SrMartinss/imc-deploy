393395005086aef68196375b950e3d9169a81001
Gabriel Martins <gabrielmartins_ninho@hotmail.com>
2024-08-26 18:32:37 -0300

    Projeto IMC

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict'
+// 1ª const - Imutável
+// 2ª let   - Mutável
+// 3ª var   - Utilize em ultimo caso
+
+const nome = document.getElementById('nome')
+nome.value = 'Gabriel'
+
+console.log(nome.value)
